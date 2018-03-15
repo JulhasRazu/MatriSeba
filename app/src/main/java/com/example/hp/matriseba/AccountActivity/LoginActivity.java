9987625599af4ef57b2594256fc8bd1e55c0abd4
hp <razu3666@gmail.com>
2018-03-15 21:49:59 +0600

    two button added

--- app/src/main/java/com/example/hp/matriseba/AccountActivity/LoginActivity.java
+++ app/src/main/java/com/example/hp/matriseba/AccountActivity/LoginActivity.java
@@ -11,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //this is a testing for github
+
     }
 }
